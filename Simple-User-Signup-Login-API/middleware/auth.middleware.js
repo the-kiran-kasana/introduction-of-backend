21eb0dd768aa803845b91bf28eaa3c9a7f5c0e4f
kkasana <kkasanacoder@gmail.com>
2025-09-17 21:30:26 +0530

    practice on authentication and authrization

---
+++ Simple-User-Signup-Login-API/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+
+
+//  const authMiddleware =  (req , res , next) =>{
+//             //check the token if token is valid  allow next
+//                     let token = req.headers?.authorization?.split(" ")[2];
+//
+//                     if(token)
+//                     {
+//                        var decoded = jwt.verify(token, 'shhhhh');
+//
+//                        console.log(decoded)
+//
+//                        if(decoded){
+//
+//                               req.user = decoded.userId;
+//                               next();
+//                               console.log("pass through auth middleware ")
+//
+//                        }else{
+//                            res.status(401).json({ msg: "try login again" });
+//                        }
+//                     } else{
+//                        res.status(401).json({ msg: "not  through auth middleware" });
+//                     }
+// }
+//
