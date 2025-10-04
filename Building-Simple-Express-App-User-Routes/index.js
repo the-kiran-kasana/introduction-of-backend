03d8f9d0bc1c60171fa45565eee361d03fbc7bbe
kkasana <kkasanacoder@gmail.com>
2025-10-04 19:49:33 +0530

    Building a Simple Express App with User Routes

--- Building-Simple-Express-App-User-Routes/index.js
+++ Building-Simple-Express-App-User-Routes/index.js
@@ -1,2 +1,19 @@
 const express = require("express")
 
+const app = express();
+
+app.get("/users/get" , (req,res) => {
+     res.json({name : "kiran kasana" , age : 20});
+})
+
+app.get("/users/list" , (req,res) => {
+     res.json([{name : "mahesh kasana" , age : 20} ,{name : "sapna kasana" , age : 20} ,{name : "suman kasana" , age : 20}] );
+})
+
+app.use((req,res) => {
+     res.json({error : "404 Not Found"});
+})
+
+app.listen(6000 , () => {
+  console.log("server is running on port 6000");
+})
